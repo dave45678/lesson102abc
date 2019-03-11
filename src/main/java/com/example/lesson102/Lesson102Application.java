1dfb4460bab096869c7254c7d7f9ce0be173aca2
GBTC408001UR <GBTC408001UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-11 14:59:13 -0400

    Initial commit

---
+++ src/main/java/com/example/lesson102/Lesson102Application.java
@@ -0,0 +1,13 @@
+package com.example.lesson102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson102Application.class, args);
+	}
+
+}
